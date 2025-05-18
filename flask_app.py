6593a21bbaa147498dd31fb2782fbcb0875a44f4
Rajesh Boppana <rajeshboppana@gmail.com>
2025-05-18 16:17:15 -0700

    adding config changes

--- flask_app.py
+++ flask_app.py
@@ -51,4 +51,4 @@ def get_config(config_id):
         return jsonify({"error": e.response['Error']['Message']}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
